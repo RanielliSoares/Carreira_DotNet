1f4c3ff2714ea537d87e1b28bf579206a3d880b7
Ranielli Soares <ranielli.pinho@outlook.com>
2024-06-11 21:53:02 -0300

    removido comentario

--- PrimeiroApp/meuPrimeiroApp/meuPrimeiroApp/Program.cs
+++ PrimeiroApp/meuPrimeiroApp/meuPrimeiroApp/Program.cs
@@ -7,7 +7,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //comentario 2
+            
         }
     }
 }
