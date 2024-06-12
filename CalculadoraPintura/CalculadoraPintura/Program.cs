f3360a18d33a7a998269320edd8cb91ff6e82fac
Ranielli Soares <ranielli.pinho@outlook.com>
2024-06-11 22:30:58 -0300

    adicionado comentario

--- CalculadoraPintura/CalculadoraPintura/Program.cs
+++ CalculadoraPintura/CalculadoraPintura/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calculo multiplicação da área
+            Console.WriteLine(2 * (5 + .3) * 2.9);
+            Console.WriteLine(5 * 3);
+
+        }
+    }
+}
